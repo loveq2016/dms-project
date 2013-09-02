04c115ed138b439439f6431cba0e75e426cff374
gaoyong <gaoyong@126.com>
2013-09-02 17:05:35 +0800

    sysuser

--- yijava-parent/yijava-dms/src/main/java/dms/yijava/api/web/system/SysFunctionController.java
+++ yijava-parent/yijava-dms/src/main/java/dms/yijava/api/web/system/SysFunctionController.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import dms.yijava.service.system.SysFunctionService;
+import dms.yijava.service.system.SysMenuFunctionService;
+import dms.yijava.service.system.SysMenuService;
 @Controller
 @RequestMapping("/sys/privilege")
 public class SysFunctionController {
 	@Autowired
-	public SysFunctionService sysFunctionService;
+	public SysMenuFunctionService sysMenuFunctionService;
+	
 }
--- yijava-parent/yijava-dms/src/main/java/dms/yijava/dao/system/SysRoleFunctionDao.java
+++ yijava-parent/yijava-dms/src/main/java/dms/yijava/dao/system/SysRoleFunctionDao.java
@@ -3,9 +3,8 @@ package dms.yijava.dao.system;
 import org.springframework.stereotype.Repository;
 
 import dms.yijava.dao.base.IbatisDaoSupport;
-import dms.yijava.entity.system.SysUser;
-
+import dms.yijava.entity.system.SysRoleFunction;
 @Repository
-public class SysUserDao extends IbatisDaoSupport<SysUser>{
-	
+public class SysRoleFunctionDao extends IbatisDaoSupport<SysRoleFunction>{
+
 }
