5c56a5d1fa0688e53d9fed64cc1670aafed35335
maxin <maxin_0408@163.com>
2013-09-02 17:34:45 +0800

    333

--- yijava-parent/yijava-dms/src/main/java/dms/yijava/dao/product/ProductCategoryDao.java
+++ yijava-parent/yijava-dms/src/main/java/dms/yijava/dao/product/ProductCategoryDao.java
@@ -1,9 +1,9 @@
-package dms.yijava.dao.productCategory;
+package dms.yijava.dao.product;
 
 import org.springframework.stereotype.Repository;
 
 import dms.yijava.dao.base.IbatisDaoSupport;
-import dms.yijava.entity.ProductCategory;
+import dms.yijava.entity.product.ProductCategory;
 
 @Repository
 public class ProductCategoryDao extends IbatisDaoSupport<ProductCategory>{
