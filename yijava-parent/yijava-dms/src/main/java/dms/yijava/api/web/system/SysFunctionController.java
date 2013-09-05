b64240928d4dd60133338d8a5a4d359ea58d8dc1
gaoyong <gaoyong@126.com>
2013-09-05 18:29:58 +0800

    fun

--- yijava-parent/yijava-dms/src/main/java/dms/yijava/api/web/system/SysFunctionController.java
+++ yijava-parent/yijava-dms/src/main/java/dms/yijava/api/web/system/SysFunctionController.java
@@ -1,15 +1,26 @@
 package dms.yijava.api.web.system;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import dms.yijava.entity.system.SysUser;
 import dms.yijava.service.system.SysMenuFunctionService;
-import dms.yijava.service.system.SysMenuService;
 @Controller
 @RequestMapping("/api/fun")
 public class SysFunctionController {
 	@Autowired
 	public SysMenuFunctionService sysMenuFunctionService;
 	
+	@RequestMapping(value = "/index")
+	public String index(HttpServletRequest request,HttpServletResponse response,ModelMap map) {
+		map.put("roleid", request.getParameter("roleid"));
+		return "forward:/system/role/viewdauthorize.jsp";
+	}	
 }
