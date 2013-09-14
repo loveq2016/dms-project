730d4033ccd093259f2f45c14ecea9f2fabcc55f
maxin <maxin_0408@163.com>
2013-09-14 16:19:05 +0800

    出货

---
+++ yijava-parent/yijava-dms/src/main/java/dms/yijava/service/deliver/DeliverDetailService.java
@@ -0,0 +1,19 @@
+package dms.yijava.service.deliver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import dms.yijava.dao.deliver.DeliverDetailDao;
+
+@Service
+@Transactional
+public class DeliverDetailService {
+
+	@Autowired
+	private DeliverDetailDao  deliverDetailDao ;
+	
+	
+	
+		
+}
