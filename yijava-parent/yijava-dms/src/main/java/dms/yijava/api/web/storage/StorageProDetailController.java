00aedb9919d5fb89777db1f7be9ab40395b6b654
maxin <maxin_0408@163.com>
2013-10-09 15:29:54 +0800

    1

--- yijava-parent/yijava-dms/src/main/java/dms/yijava/api/web/storage/StorageProDetailController.java
+++ yijava-parent/yijava-dms/src/main/java/dms/yijava/api/web/storage/StorageProDetailController.java
@@ -4,14 +4,19 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.yijava.orm.core.JsonPage;
+import com.yijava.orm.core.PageRequest;
 import com.yijava.orm.core.PropertyFilter;
 import com.yijava.orm.core.PropertyFilters;
 
 import dms.yijava.entity.storage.StorageProDetail;
+import dms.yijava.entity.system.SysUser;
 import dms.yijava.service.storage.StorageProDetailService;
 
 @Controller
@@ -26,6 +31,22 @@ public class StorageProDetailController {
 		//HashMap<String,String> parameters = new HashMap<String,String>();
 		return storageProDetailService.getList(filters);
 	}
+	
+	
+	
+	@ResponseBody
+	@RequestMapping("paging")
+	public JsonPage<StorageProDetail> paging(PageRequest pageRequest,
+			HttpServletRequest request) {
+		List<PropertyFilter> filters = PropertyFilters.build(request);
+		SysUser sysUser = (SysUser) request.getSession().getAttribute("user");
+		if(!StringUtils.equals("0",sysUser.getFk_dealer_id())){
+			filters.add(PropertyFilters.build("ANDS_fk_dealer_id",sysUser.getFk_dealer_id()));
+		}
+		return storageProDetailService.paging(pageRequest, filters);
+	}
+	
+	
 	
 
 }
