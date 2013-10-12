0cefc95a8c8985e29d9d7cf22c75b1e547f87bbb
gaoyong <gaoyong@126.com>
2013-10-12 16:48:15 +0800

    log

--- yijava-parent/yijava-dms/src/main/java/dms/yijava/api/web/filter/LogFilter.java
+++ yijava-parent/yijava-dms/src/main/java/dms/yijava/api/web/filter/LogFilter.java
@@ -8,10 +8,8 @@ import ch.qos.logback.core.spi.FilterReply;
 public class LogFilter extends LevelFilter {
 	@Override
 	public FilterReply decide(ILoggingEvent event) {
-	    LoggingEvent even2t = (LoggingEvent)event;  
-
 		 if (String.valueOf(event.getLevel()).equalsIgnoreCase("INFO") && 
-				 event.getLoggerName().contains("dms.yijava.api.web.")) {
+				 event.getLoggerName().startsWith("dms.yijava.api.web.")) {
 		     return FilterReply.ACCEPT;
 		 }else {
 		     return FilterReply.DENY;
