c4ce130eb8079d3733849a8113d512d12a8af122
zhjt <zhjt_88@163.com>
2013-10-23 23:12:02 +0800

    增加2个提醒业务，发货提醒和收货提醒

--- yijava-parent/yijava-dms/src/main/java/dms/yijava/event/handler/UserCheckFlowEventHandler.java
+++ yijava-parent/yijava-dms/src/main/java/dms/yijava/event/handler/UserCheckFlowEventHandler.java
@@ -1,20 +1,28 @@
 package dms.yijava.event.handler;
 
+import java.util.Date;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
 import com.google.common.eventbus.Subscribe;
+import com.yijava.common.utils.DateUtils;
 
+import dms.yijava.entity.flow.FlowRecord;
+import dms.yijava.entity.system.SysUser;
 import dms.yijava.event.EventDriven;
 import dms.yijava.event.UserCheckFlowEvent;
 import dms.yijava.service.adjuststorage.AdjustStorageService;
 import dms.yijava.service.deliver.DeliverService;
 import dms.yijava.service.exchanged.ExchangedService;
+import dms.yijava.service.flow.FlowRecordService;
 import dms.yijava.service.order.OrderService;
 import dms.yijava.service.pullstorage.PullStorageService;
 import dms.yijava.service.pullstorage.SalesStorageService;
+import dms.yijava.service.system.SysUserService;
 import dms.yijava.service.trial.TrialService;
 
 @EventDriven
@@ -39,6 +47,11 @@ public class UserCheckFlowEventHandler {
 	private ExchangedService exchangedService;
 	
 	
+	@Autowired
+	private FlowRecordService flowRecordService;
+	@Autowired
+	private SysUserService sysUserService;
+	
 	@Value("#{properties['trialflow_identifier_num']}")   	
 	private String flowIdentifierNumber;
 	@Value("#{properties['deliverflow_identifier_num']}")   	
@@ -54,7 +67,13 @@ public class UserCheckFlowEventHandler {
 	@Value("#{properties['exchangedflow_identifier_num']}")   	
 	private String exchangedflow_identifier_num;
 	
+	//发货提醒
+	@Value("#{properties['sendproduct_identifier_num']}")   	
+	private String sendproduct_identifier_num;
 	
+	//收货提醒
+	@Value("#{properties['reciveproduct_identifier_num']}")   	
+	private String reciveproduct_identifier_num;
 	
 	@Subscribe
 	public void onUserCheckAgree(UserCheckFlowEvent flow_check) {
@@ -69,6 +88,24 @@ public class UserCheckFlowEventHandler {
 			//发货单
 			logger.debug("发货流程审核完毕,更新状态,业务号:"+flow_check.getBussiness_id());
 			deliverService.updateDeliverStatus(flow_check.getBussiness_id(), "3");//已经审核
+			
+			//增加待处理事项,只做提醒
+			 List<SysUser> users=sysUserService.getListByDepartmentId("86");
+			 if(users!=null && users.size()>0)
+			 {
+			 	FlowRecord entity=new FlowRecord();
+				entity.setFlow_id(sendproduct_identifier_num);
+				entity.setBussiness_id(flow_check.getBussiness_id());		
+				entity.setTitle("待发货");
+				entity.setSend_id("0000000000");//只做提醒，所以此处设置了全0
+				entity.setCheck_id(users.get(0).getId());
+				entity.setSend_time(DateUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss"));
+				entity.setCreate_time(DateUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss"));			
+				entity.setStatus("0");
+				//entity.setStep_order_no(step_order_no);
+				flowRecordService.saveEntity(entity);
+			 }
+			
 		}else if(flow_check.getFlow_id().equals(orderflow_identifier_num)){
 			//订单
 			logger.debug("订单流程审核完毕,更新状态,业务号:"+flow_check.getBussiness_id());
