adb42107c8e81444b5591c356fbdeb7dc04a9602
gaoyong <gaoyong@gg.com>
2013-11-09 01:30:57 +0800

    产品分类激活医院报表

---
+++ yijava-parent/yijava-dms/src/main/java/dms/yijava/dao/repost/RepostDao.java
@@ -0,0 +1,12 @@
+package dms.yijava.dao.repost;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import dms.yijava.dao.base.IbatisDaoSupport;
+
+@Repository
+public class RepostDao extends IbatisDaoSupport<Map<String,Object>>{
+
+}
